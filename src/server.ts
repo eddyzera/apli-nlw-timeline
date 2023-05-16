6c80ea548af93d97c755c717662cb3bb99bfa19b
eddyzera <esilva644@gmail.com>
2023-05-16 10:29:18 -0300

    chore: configuration eslint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-  port: 3333
-}).then(server => console.log(`⭐️ http server running on http://localhost:${server}`))
+app
+  .listen({
+    port: 3333,
+  })
+  .then((server) =>
+    console.log(`⭐️ http server running on http://localhost:${server}`),
+  )
